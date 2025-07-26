f3a7034f8a22b259b9beeae11ef21d6738d0211d
Logan Sobczak <logan.sobczak@instant.co>
2025-07-26 08:38:58 -0400

    feat: add colors, spacing, version lock, empty folders

---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+export type HexCode = string;
+
+export const colors: Record<string, HexCode> = {
+  // Generated from https://coolors.co
+  primary: "#3A86FF", // Azure...which I just learned you're supposed to pronounce AZH-ur
+  secondary: "#32161F", // Dark Purple
+  textPrimary: "#121619", // Night
+  success: "#3C896D", // Viridian
+  warning: "FFBE0B", // Amber
+  error: "#AA5042", // Redwood - love the names it comes up with
+};
